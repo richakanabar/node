d9975788f1f18e198e9305a4e01a9921e04027f4
richakanabar <richakanabar14@gmail.com>
2025-10-04 13:05:20 +0530

    Update index.js

--- index.js
+++ index.js
@@ -5,9 +5,10 @@ const app = express();
 app.use(express.json());
 
 
-app.use("/student",studentRoute);
+app.use("Helloooo",);
 
 
 app.listen(3000,() => {
     console.log(`Server is running at http://localhost:/`)
+
 });
